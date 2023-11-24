98f6181f944fc7a6e960fd49cb4edc2b60fb849a
KG <50565248+yusukeKG@users.noreply.github.com>
2023-11-24 14:54:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,14 @@ x = 10
 
 import streamlit as st
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+
+import pandas as pd
+if uploaded_file is not None:
+  df = pd.read_csv(
+    uploaded_file,
+    sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+    engine='python',
+    na_values='-',
+    header=None)
+  st.markdown('### アクセスログ（先頭5件）')
+  st.write(df.head(5))
